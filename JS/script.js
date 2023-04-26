3f1b84ad2e88151ca08129a8695f4c6717bc869a
Antczh <ant.c.h@hotmail.co.uk>
2023-04-26 10:55:47 +0100

    imgs added

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,17 @@ gridItems.forEach((item) => {
   });
 });
 
+const winnerCombos = [
+  [box1, box2, box3],
+  [box4, box5, box6],
+  [box7, box8, box9],
+  [box1, box4, box7],
+  [box2, box5, box8],
+  [box3, box6, box9],
+  [box1, box5, box9],
+  [box3, box5, box7],
+];
+
 function showX() {}
 
 function showO() {}
