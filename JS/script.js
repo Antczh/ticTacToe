6e64be14f3eb5d23049f8656950236624d36a9b5
Antczh <ant.c.h@hotmail.co.uk>
2023-05-08 19:26:05 +0100

    tracking where x and o goes on the board

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,35 @@
-const winnerCombos = [
-  [box1, box2, box3],
-  [box4, box5, box6],
-  [box7, box8, box9],
-  [box1, box4, box7],
-  [box2, box5, box8],
-  [box3, box6, box9],
-  [box1, box5, box9],
-  [box3, box5, box7],
+// const winnerCombos = [
+//   [box1, box2, box3],
+//   [box4, box5, box6],
+//   [box7, box8, box9],
+//   [box1, box4, box7],
+//   [box2, box5, box8],
+//   [box3, box6, box9],
+//   [box1, box5, box9],
+//   [box3, box5, box7],
+// ];
+
+const board = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 const gridItems = document.querySelectorAll(".gridBox");
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomEmptyCell(max) {
+  let row, col;
+  while (!row) {
+    let randomRow = Math.floor(Math.random() * 2);
+    let randomCol = Math.floor(Math.random() * 2);
+
+    if (board[randomRow][randomCol] == null) {
+      row = randomRow;
+      col = randomCol;
+    }
+  }
+
+  return [row, col];
 }
 
 function showX(event) {
@@ -22,6 +39,9 @@ function showX(event) {
   imgX.classList.add("x-image");
   imgX.classList.add("appendedImg");
   cell.appendChild(imgX);
+
+  board[cell.getAttribute("data-row")][cell.getAttribute("data-col")] = "X";
+  console.log(board);
 }
 
 function showO(cell) {
@@ -30,15 +50,21 @@ function showO(cell) {
   imgO.classList.add("o-image");
   imgO.classList.add("appendedImg");
   cell.appendChild(imgO);
+
+  board[cell.getAttribute("data-row")][cell.getAttribute("data-col")] = "O";
+  console.log(board);
 }
 
 gridItems.forEach((item) => {
   item.addEventListener("click", (event) => {
     showX(event);
     console.log("Cell clicked");
-    // computer's move - generate random cell index
-    const randomIndex = getRandomInt(gridItems.length);
-    showO(gridItems[randomIndex]);
+    const randomIndex = getRandomEmptyCell(gridItems.length);
+    console.log(randomIndex);
+    setTimeout(function () {
+      let cell = document.querySelector("[data-row=0][data-col=0]");
+      showO(gridItems[randomIndex]);
+    }, 1000);
   });
 });
 
@@ -51,3 +77,5 @@ function playNewGame() {
 const playAgainBtn = document
   .getElementById("playAgain")
   .addEventListener("click", playNewGame);
+
+//
