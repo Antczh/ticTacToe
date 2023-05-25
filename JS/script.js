fa16625146176cb6e2b53bfbdf1124edee59a7d5
Antczh <ant.c.h@hotmail.co.uk>
2023-05-25 18:32:42 +0100

    playAgain button restarts checkWinner function

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,3 @@
-// const winnerCombos = [
-//   [box1, box2, box3],
-//   [box4, box5, box6],
-//   [box7, box8, box9],
-//   [box1, box4, box7],
-//   [box2, box5, box8],
-//   [box3, box6, box9],
-//   [box1, box5, box9],
-//   [box3, box5, box7],
-// ];
-
 const winnerCombos = [
   [
     [true, true, true],
@@ -188,6 +177,7 @@ function checkWinner() {
   });
   if (winnerDecided) {
     console.log("There is a winner");
+    return;
   }
 }
 
@@ -195,6 +185,8 @@ function playNewGame() {
   const grid = document.querySelector(".grid-container");
   const appendedImages = document.querySelectorAll(".appendedImg");
   appendedImages.forEach((img) => img.remove());
+
+  checkWinner();
 }
 
 const playAgainBtn = document
